327ecff4e036f79ffd4ab762cf383eacd5802278
saranshgrover <ycubiksrube@gmail.com>
2020-05-16 07:45:55 -0400

    :lipstick: Use dark theme and teal primary color

---
+++ src/FeatureList.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import Divider from '@material-ui/core/Divider'
+import EmojiSymbolsIcon from '@material-ui/icons/EmojiSymbols'
+import EmojiObjectsIcon from '@material-ui/icons/EmojiObjects'
+import KeyboardIcon from '@material-ui/icons/Keyboard'
+import Typography from '@material-ui/core/Typography'
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		width: '100%',
+	},
+}))
+
+export default function SimpleList() {
+	const classes = useStyles()
+
+	return (
+		<div className={classes.root}>
+			<List component='nav'>
+				<ListItem>
+					<ListItemIcon color='primary'>
+						<EmojiObjectsIcon />
+					</ListItemIcon>
+					<ListItemText
+						primary='Remember your Emoji use and frequency'
+						secondary='Emoji Tree remembers how often you use certain emojis and what keywords you use to reference emojis 🤯 It always recommends the most frequent emojis for that given keyword'
+					/>
+				</ListItem>
+				<ListItem>
+					<ListItemIcon>
+						<EmojiSymbolsIcon />
+					</ListItemIcon>
+					<ListItemText
+						primary='Add Aliases for all Emojis!'
+						secondary='Emoji Tree lets you add keywords for all emojis and look up those emojis with these aliases, because nobody uses emojis like you do 😜'
+					/>
+				</ListItem>
+				<ListItem>
+					<ListItemIcon>
+						<KeyboardIcon />
+					</ListItemIcon>
+					<ListItemText
+						primary='Super easy Emoji Input'
+						secondary='You should not have to leave your keyboard to search for emojis. With EmojiTree, simply type a colon (:) and start searching for an emoji. Once you find the perfect 💯 emoji, enter the number that is next to the emoji, and continue typing the very important message! 👩‍🏫'
+					/>
+				</ListItem>
+			</List>
+			<Typography align='center' variant='body1'>
+				To get started, start typing in the text field!
+			</Typography>
+		</div>
+	)
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+import teal from '@material-ui/core/colors/teal'
+import green from '@material-ui/core/colors/green'
+import CssBaseline from '@material-ui/core/CssBaseline'
+const theme = createMuiTheme({
+	palette: {
+		primary: teal,
+		secondary: green,
+		type: 'dark',
+	},
+	status: {
+		danger: 'orange',
+	},
+})
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+	<React.StrictMode>
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<App />
+		</ThemeProvider>
+	</React.StrictMode>,
+	document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
